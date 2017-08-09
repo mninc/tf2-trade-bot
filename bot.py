949b8e60fc216e82fd2c2c72d0eba107ce9ac807
Nate the great <zwork101@gmail.com>
2017-08-09 13:26:04 -0400

    The update update!
    bot.py will now ask if you would wish to update, and update for you!

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ key_price = 0
 bud_price = 0
 escrow = None
 currencies = {'bud':'Earbuds', 'ref':'Refined Metal', 'rec':'Reclaimed Metal', 'scrap':'Scrap Metal', 'key':'Mann Co. Supply Crate Key'}
-packages = {'steampy':'0.42', 'requests':'2.18.1', 'bs4':'4.6.0'}
+packages = ['steampy', 'requests', 'bs4', 'backpackpy']
 declined_trades = []
 
 start_text = """
@@ -29,6 +29,30 @@ start_text = """
 Created by: Zwork101    Github: https://github.com/Zwork101    Steam: https://steamcommunity.com/id/ZWORK101\n
 """
 
+def check_for_updates():
+    with open('__version__', 'r') as file:
+        curr_version = file.read()
+    r = requests.get('https://raw.githubusercontent.com/Zwork101/tf2-trade-bot/master/__version__')
+    new_version = r.text
+    cmajor, cminor, cpatch = [int(i) for i in curr_version.split('.')]
+    nmajor, nminor, npatch = [int(i) for i in new_version.split('.')]
+    totalC, totalN = ((cmajor*100) + (cminor*10) + cpatch), ((nmajor*100) + (nminor*10) + npatch)
+    if totalN > totalC:
+        print('[PROGRAM]: New version is available, would you like to install?')
+        yn = input('[y/n]: ')
+        if yn[0].lower() == 'y':
+            print('[Installer]: Starting installation...', end='')
+            bot_update = requests.get('https://raw.githubusercontent.com/Zwork101/tf2-trade-bot/master/bot.py')
+            with open('__version__', 'w') as file:
+                file.write(new_version)
+                print('.', end='')
+            with open('bot.py', 'w') as file:
+                file.write(bot_update.text)
+                print('.')
+            print('Update complete! Restart now.')
+            input('press enter to close program...\n')
+            os._exit(0)
+
 def calculate(scrapC, recC, refC, keyC, budC):
     BeyC = budC * bud_price
     refK = (BeyC * key_price) + (keyC * key_price)
@@ -36,13 +60,15 @@ def calculate(scrapC, recC, refC, keyC, budC):
     totalR += refK
     return totalR
 
-def check_install(pkg, v, c):
+def check_install(pkg, c, imp=''):
     try:
         exec(f'import {pkg}')
         print(f'[PROGRAM]: Required package is installed {c}/3')
     except ImportError:
+        if not imp:
+            pkg = imp
         print('[PROGRAM]: A required package is not installed, installing...')
-        r = os.popen(f'{os.path.dirname(sys.executable)}\\python.exe -m pip install {pkg}=={v}')
+        r = os.popen(f'{os.path.dirname(sys.executable)}\\python.exe -m pip install {pkg}')
         print('[PROGRAM]: Installed package! Please restart this program to continue.')
         input('press enter to close program...\n')
         os._exit(0)
@@ -127,31 +153,15 @@ if __name__ == '__main__':
     print(start_text)
 
     for pkg in packages:
-        check_install(pkg, packages[pkg], list(packages.keys()).index(pkg))
+        check_install(pkg, packages.index(pkg), '' if pkg!='backpackpy' else 'backpack.py')
 
     from bs4 import BeautifulSoup
     from steampy.client import SteamClient
     from steampy.exceptions import InvalidCredentials
+    from backpackpy import listings
+    import requests
 
-    try:
-        import requests
-        print('[PROGRAM]: Required package is installed 2/3')
-    except ImportError:
-        print('[PROGRAM]: A required package is not installed, installing...')
-        r = os.popen(f'{os.path.dirname(sys.executable)}\\python.exe -m pip install requests==2.18.1')
-        print('[PROGRAM]: Installed package! Please restart this program to continue.')
-        input('press enter to close program...\n')
-        os._exit(0)
-
-    try:
-        import requests
-        print('[PROGRAM]: Required package is installed 3/3')
-    except ImportError:
-        print('[PROGRAM]: A required package is not installed, installing...')
-        r = os.popen(f'{os.path.dirname(sys.executable)}\\python.exe -m pip install bs4==4.6.0')
-        print('[PROGRAM]: Installed package! Please restart this program to continue.')
-        input('press enter to close program...\n')
-        os._exit(0)
+    check_for_updates()
 
     try:
         with open('settings.json', 'r') as cfg:
@@ -208,12 +218,9 @@ if __name__ == '__main__':
             for line in file:
                 try:
                     item, price, typ = line.split(',')
-                    print("split")
                     item, price, typ = item.strip(), price.strip(), typ.strip()
-                    print('stripped')
                     if typ[0].lower() == 's':
                         sell_trades[item] = float(price)
-                        print(sell_trades)
                     else:
                         buy_trades[item] = float(price)
                 except ValueError:
